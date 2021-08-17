2531bdf40eed3dcc55e6a4fc9b41250cfa440519
Chandan Chainani <cchainanichan@gmail.com>
2021-08-17 12:14:24 +0530

    added exception handling in lifts.py

--- lifts.py
+++ lifts.py
@@ -14,36 +14,38 @@ while len(lifts) < MAX_LIFTS:
 lifts = list(lifts.keys())
 print("> lift_position = ", lifts)
 
-u_position, u_direction = DIGIT_WORD_REGEX.search(input("> Enter a request? ")).groups()
-u_position = int(u_position)
-lift_position = ''
-min_distance = None
-for lift_index in range(len(lifts)):
-    l_position, l_direction = DIGIT_WORD_REGEX.search(lifts[lift_index]).groups()
-    l_position = int(l_position)
-    distance = l_position - u_position
-    if l_direction == None:
-        distance = abs(distance)
-    elif l_direction != u_direction:
-        if l_direction == 'D':
-            distance = l_position + u_position
-        elif l_direction == 'U':
-            distance = abs(MAX_FLOORS - l_position) + MAX_FLOORS
-    elif l_direction == u_direction:
-        if l_direction == 'D':
-            if l_position > u_position:
-                distance = abs(distance)
-            elif l_position < u_position:
-                distance = l_position + u_position + abs(distance)
-        if l_direction == 'U':
-            if l_position < u_position:
-                distance = abs(distance)
-            elif l_position > u_position:
-                distance = l_position + u_position + abs(distance)
+try:
+    u_position, u_direction = DIGIT_WORD_REGEX.search(input("> Enter a request? ")).groups()
+    u_position = int(u_position)
+    lift_position = ''
+    min_distance = None
+    for lift_index in range(len(lifts)):
+        l_position, l_direction = DIGIT_WORD_REGEX.search(lifts[lift_index]).groups()
+        l_position = int(l_position)
+        distance = l_position - u_position
+        if l_direction == None:
+            distance = abs(distance)
+        elif l_direction != u_direction:
+            if l_direction == 'D':
+                distance = l_position + u_position
+            elif l_direction == 'U':
+                distance = abs(MAX_FLOORS - l_position) + MAX_FLOORS
+        elif l_direction == u_direction:
+            if l_direction == 'D':
+                if l_position > u_position:
+                    distance = abs(distance)
+                elif l_position < u_position:
+                    distance = l_position + u_position + abs(distance)
+            if l_direction == 'U':
+                if l_position < u_position:
+                    distance = abs(distance)
+                elif l_position > u_position:
+                    distance = l_position + u_position + abs(distance)
 
 
-    if min_distance == None or min_distance < 0 or min_distance > distance:
-        min_distance, lift_position = distance, lift_index
+        if min_distance == None or min_distance < 0 or min_distance > distance:
+            min_distance, lift_position = distance, lift_index
 
-print("Lift #{} will be comming up to receive you".format(lift_position + 1))
-
+    print("Lift #{} will be comming up to receive you".format(lift_position + 1))
+except Exception as e:
+    print("Something went wrong")
